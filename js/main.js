10da822efd1c5f991059b5560e1e1d4e3cb72606
Alex Dediu <adediu25@gmail.com>
2024-02-13 19:44:24 -0500

    working choropleth map
    Map is working and displaying, still need to resolve scaling and filtering

--- js/main.js
+++ js/main.js
@@ -1,15 +1,51 @@
-d3.csv('data/national_health_data.csv')
-    .then(data => {
-        // work with data
-        data.forEach(d => {
+Promise.all([
+    d3.json('data/usa_counties.json'),
+    d3.csv('data/national_health_data.csv')
+]).then(data => {
+    let geoData = data[0];
+    let countyData = data[1];    
+
+        
+        // set up county data
+        countyData.forEach(d => {
+            d.air_quality = +d.air_quality;
+            d.display_name = d.display_name.slice(1,-1);
+            d.education_less_than_high_school_percent = +d.education_less_than_high_school_percent;
+            d.poverty_perc = +d.poverty_perc;
+            d.elderly_percentage = +d.elderly_percentage;
+            d.number_of_hospitals = +d.number_of_hospitals;
             d.median_household_income = +d.median_household_income;
+            d.number_of_primary_care_physicians = +d.number_of_primary_care_physicians;
+            d.percent_coronary_heart_disease = +d.percent_coronary_heart_disease;
             d.park_access = +d.park_access;
-            console.log(d);
+            d.percent_coronary_heart_disease = +d.percent_coronary_heart_disease;
+            d.median_household_income = +d.median_household_income;
+            d.percent_high_blood_pressure = +d.percent_high_blood_pressure;
+            d.percent_high_cholesterol = +d.percent_high_cholesterol;
+            d.percent_inactive = +d.percent_inactive;
+            d.percent_no_heath_insurance = +d.percent_no_heath_insurance;
+            d.percent_smoking = +d.percent_smoking;
+            d.percent_stroke = +d.percent_stroke;
         });
 
-        const scatterplot = new Scatterplot({parentElement: '#chart-area'}, data);
+        console.log(countyData[0]);
+        // Add county data to respective county in TopoJSON
+        geoData.objects.counties.geometries.forEach(d => {
+            // county_data will stay null if we do not have that county in the data
+            d.properties.county_data = null
+            for (let i = 0; i < countyData.length; i++) {
+                if (d.id == countyData[i].cnty_fips) {
+                    d.properties.county_data = countyData[i];
+                }
+            }
+        });
 
-        scatterplot.updateVis()
+        const choroplethMap = new ChoroplethMap({
+            parentElement: '#map'
+        }, geoData);
+
+        const scatterplot = new Scatterplot({parentElement: '#chart-area'}, countyData);
+        scatterplot.updateVis();
     })
     .catch(error => {
         console.error('Error:');
